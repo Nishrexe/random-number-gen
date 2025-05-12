33b16cd862631ea01486d3d792729ad1d5bbd9c7
nishrexe <nischalreddy.uk@gmail.com>
2025-05-12 02:42:03 -0700

    odd or even checker

--- random_generator.py
+++ random_generator.py
@@ -5,5 +5,10 @@ random_number = random.randint(1, 200)
 
 print("Your random number is:", random_number)
 
+# Check if the number is even or odd
+if random_number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
 
 
